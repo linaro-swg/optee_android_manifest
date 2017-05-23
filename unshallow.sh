6336eaf287d8848079ae3d7be9b0271bb602c08a
Victor Chong <victor.chong@linaro.org>
2017-05-23 14:50:11 +0100

    Add echo to unshallow.sh
    Signed-off-by: Victor Chong <victor.chong@linaro.org>

--- unshallow.sh
+++ unshallow.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+echo "unshallow android-patchsets"
 cd ~/work/swg/svp/hikey-n-4.9/android-patchsets
 git fetch linaro-android --unshallow
+echo "unshallow dlh"
 cd ~/work/swg/svp/hikey-n-4.9/device/linaro/hikey
 git fetch linaro-android --unshallow
+echo "unshallow kernel"
 cd ~/work/swg/svp/hikey-n-4.9/kernel/linaro/hisilicon
 git fetch aosp --unshallow
+echo "unshallow xtest"
 cd ~/work/swg/svp/hikey-n-4.9/external/optee_test
 git fetch github --unshallow
+echo "unshallow client"
 cd ~/work/swg/svp/hikey-n-4.9/external/optee_client
 git fetch github --unshallow
+echo "unshallow hello world"
 cd ~/work/swg/svp/hikey-n-4.9/external/optee_hello_world
 git fetch github --unshallow
+echo "unshallow optee_os"
 cd ~/work/swg/svp/hikey-n-4.9/optee/optee_os
 git fetch github --unshallow
