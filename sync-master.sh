5fa86673e794b925b32a62332f6c2ccab3fb3ac3
Victor Chong <victor.chong@linaro.org>
2018-06-29 14:35:54 +0100

    mkdir -p logs in sync-<version>.sh
    Signed-off-by: Victor Chong <victor.chong@linaro.org>

--- sync-master.sh
+++ sync-master.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mkdir -p logs
 ./sync.sh -d 2>&1 |tee logs/sync-master.log
 #./sync.sh -bm pinned-manifest_YYYYMMDD-HHMM -d 2>&1 |tee logs/sync-master.log
 #./sync.sh -j 24 -d 2>&1 |tee logs/sync-master.log
--- sync-p.sh
+++ sync-p.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mkdir -p logs
 ./sync.sh -v p -d 2>&1 |tee logs/sync-p.log
 #./sync.sh -v p -bm pinned-manifest_YYYYMMDD-HHMM -d 2>&1 |tee logs/sync-p.log
 #./sync.sh -v p -j 24 -d 2>&1 |tee logs/sync-p.log
