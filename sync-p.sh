071e166d8150ab0b13c01597fbd8c183264fa64c
Victor Chong <victor.chong@linaro.org>
2019-01-29 05:48:23 +0000

    make changes for 3.4.0 release

--- sync-p.sh
+++ sync-p.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 mkdir -p logs
-./sync.sh -v p 2>&1 |tee logs/sync-p.log
+./sync.sh -v p -bm pinned-manifest_hikey_v9r30_v3.4.0_20190128.xml 2>&1 |tee logs/sync-p.log
+#./sync.sh -v p 2>&1 |tee logs/sync-p.log
