2887c743557b1f88190632bf453776fd3fb4aa6d
Victor Chong <victor.chong@linaro.org>
2019-09-09 19:47:01 +0100

    sync-p*.sh: enable dbg logs for releases
    Signed-off-by: Victor Chong <victor.chong@linaro.org>

--- sync-p.sh
+++ sync-p.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./sync.sh -v p -bm pinned-manifest_hikey_v9r34_v3.6.1_20190903.xml "$@" 2>&1 |tee logs/sync-p.log
+./sync.sh -v p -d -bm pinned-manifest_hikey_v9r34_v3.6.1_20190903.xml "$@" 2>&1 |tee logs/sync-p.log
 #./sync.sh -v p "$@" 2>&1 |tee logs/sync-p.log
