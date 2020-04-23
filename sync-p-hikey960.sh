4ae8b8a97ccbe715ed02433d05df086e6bee4f15
Victor Chong <victor.chong@linaro.org>
2020-04-23 09:26:16 +0100

    3.7.0
    Signed-off-by: Victor Chong <victor.chong@linaro.org>

--- sync-p-hikey960.sh
+++ sync-p-hikey960.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./sync.sh -v p -d -t hikey960 -bm pinned-manifest_hikey960_v9r34_v3.6.1_20190903.xml "$@" 2>&1 |tee logs/sync-p.log
+./sync.sh -v p -d -t hikey960 -bm pinned-manifest_hikey960_v9r34_v3.7.0_20200423.xml "$@" 2>&1 |tee logs/sync-p.log
 #./sync.sh -v p -t hikey960 "$@" 2>&1 |tee logs/sync-p.log
