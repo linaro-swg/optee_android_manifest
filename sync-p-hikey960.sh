628162fe54b1fc995c9176d768700e48de3688fd
Victor Chong <victor.chong@linaro.org>
2021-04-20 17:34:05 +0100

    3.13.0
    optee_client and optee_test are on 3.13.0 + patches to support the
    plugin framework for AOSP builds.
    
    Regression in pkcs11_1014.

--- sync-p-hikey960.sh
+++ sync-p-hikey960.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./sync.sh -v p -t hikey960 -bm pinned-manifest_hikey960_v9r30_v3.12.0_20210128.xml 2>&1 |tee logs/sync-p.log
+./sync.sh -v p -t hikey960 -bm pinned-manifest_hikey960_v9r30_v3.13.0_20210421.xml 2>&1 |tee logs/sync-p.log
 #./sync.sh -v p -t hikey960 2>&1 |tee logs/sync-p.log
